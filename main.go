109277d43a549aacc7b67db1e3322440e97b8a14
Cale Govindasami <calegovindasami@gmail.com>
2025-04-25 09:15:00 +0200

    added root endpoint

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	http.HandleFunc("/", root)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server has been shutdown\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("request: GET /\n")
+	io.WriteString(w, "This is my server root\n")
 }
